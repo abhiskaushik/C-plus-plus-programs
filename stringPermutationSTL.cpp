585ef51d07a00546791d67ac70927fef9742e7a7
Abhishek Kaushik <stndlkr200@gmail.com>
2014-11-25 02:25:07 +0530

    permutations of a string using STL

---
+++ stringPermutationSTL.cpp
@@ -0,0 +1,24 @@
+/*
+String Permutation using STL..
+
+
+*/
+#include<algorithm>
+#include<string>
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+
+  string s = "aab";
+
+  sort(s.begin(), s.end());
+    do {
+
+  cout << s << '\n';
+
+}while(next_permutation(s.begin(), s.end()));
+
+return 0;
+}
