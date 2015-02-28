4004fc20b4b0e948e1afcb9f1160ff97420d5b7c
stndlkr200 <stndlkr200@gmail.com>
2015-02-28 18:02:25 +0530

    class to reverse a string without stack

---
+++ stack9.cpp
@@ -0,0 +1,59 @@
+/*
+reversing a string without using extra space of stack..
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Reverse{
+  private:
+  	int i,j;
+  
+  public:
+      
+      //Reverse(){
+      	//i=0;
+      //}
+
+    void swap(char *a,char *b){
+         char temp=*a;
+         *a=*b;
+         *b=temp;
+    }
+
+  	string rev(string str){
+       i=0,j=str.length()-1;
+       while(i<j){
+       	swap(&str[i],&str[j]);
+        i++;
+        j--;
+       }
+       return str;
+  	}
+
+
+};
+/*
+void swap(char *a,char *b){
+	char temp=*a;
+	*a=*b;
+	*b=temp;
+} */
+int main(){
+    
+    Reverse r;
+    string s;
+    cin>>s;
+
+   string t=r.rev(s);
+   /*int i=0,j=s.length()-1;
+    while(i<j){
+    	swap(&s[i],&s[j]);
+    	i++; j--;
+    }
+    */
+   
+	cout<<t<<endl;
+
+}
