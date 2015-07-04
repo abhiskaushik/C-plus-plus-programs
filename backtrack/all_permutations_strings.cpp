06d25b1309d5c69734e7d7027872ee5515d7677a
stndlkr200 <stndlkr200@gmail.com>
2015-07-04 16:19:04 +0530

    backtracks

---
+++ backtrack/all_permutations_strings.cpp
@@ -0,0 +1,36 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+
+      void swap(char *x, char *y){
+        char temp=*x;
+        *x=*y;
+        *y=temp;
+      }
+
+void permute(char *a,int l,int r){
+	if(l==r)
+		cout<<a<<endl;
+     else
+     {
+     	for (int i = l; i <=r ; ++i)
+     	{
+     		/* code */
+     		swap((a+l),(a+i));
+     		permute(a,l+1,r);
+     		swap((a+l),(a+i)); //backtrack
+     	}
+     }
+
+
+}
+
+  int main(int argc, char const *argv[])
+  {
+
+  	string s;
+  	cin>>s;
+  	int l=s.length();
+  	permute(&s[0],0,l-1);
+
+  	return 0;
+  }
