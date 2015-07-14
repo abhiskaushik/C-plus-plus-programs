d01ee42558b787d2e67cb78c8abc6bee5334a583
stndlkr200 <stndlkr200@gmail.com>
2015-07-14 09:48:43 +0530

    list rep

---
+++ graphs/graph_list.cpp
@@ -0,0 +1,46 @@
+  #include<bits/stdc++.h>
+   using namespace std;
+
+
+   int main(int argc, char const *argv[])
+   {    
+   	   int n;
+   	   cin>>n;
+   	   vector< list<int> > adjList(n);
+
+   	   adjList[0].push_back(0);
+   	   adjList[0].push_back(1);
+
+   	   adjList[1].push_back(2);
+   	   adjList[1].push_back(0);
+
+   	   adjList[2].push_back(0);
+   	   adjList[2].push_back(1);
+   	   adjList[2].push_back(3);
+
+   	   adjList[3].push_back(2);
+
+   	   //print the graph..
+
+   	   int c=0;
+   	     vector<list<int> >::iterator i;
+
+   	   for ( vector<list<int> >::iterator i =adjList.begin(); i!=adjList.end(); ++i)
+   	   {
+   	   	/* code */  
+   	   	    cout<<"vertices connected to node "<<c<<"are";
+   	   	      list<int> li=*i;
+   	   	   for (list<int >::iterator itr=li.begin(); itr!=li.end() ; ++itr)
+   	   	   {
+   	   	   	/* code */
+   	   	   	  cout<< *itr <<" ";
+
+   	   	   }
+   	   	   cout<<endl;
+   	   	   c++;
+   	   }
+
+
+
+   	return 0;
+   }
