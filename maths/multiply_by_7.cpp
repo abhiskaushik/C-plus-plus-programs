5f8cc1e22eda7a094a7a9bc50eecd2c615a44fa4
stndlkr200 <stndlkr200@gmail.com>
2015-07-29 13:42:34 +0530

    multiply by 7 in o(1)

---
+++ maths/multiply_by_7.cpp
@@ -0,0 +1,12 @@
+  #include<bits/stdc++.h>
+   using namespace std;
+
+
+   int main(int argc, char const *argv[])
+   {
+   	 int n;
+   	 cin>>n;
+
+   	 cout<<((n<<3)-n)<<endl;
+   	return 0;
+   }
